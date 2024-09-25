6d7aac080ce246c92e3019d7aa0bf036284f1795
JJ <jeremyjacob101@gmail.com>
2024-09-25 14:08:26 +0300

    got rid of favorites/local storage from youtube video. go back here if you want to see that stuff

--- src/App.js
+++ src/App.js
@@ -1,56 +1,11 @@
 import React, { useState, useEffect } from 'react';
-// import MovieList from './components/MovieList';
-// import VerticalFavorites from './components/VerticalFavorites';
-// import SearchBox from './components/SearchBox';
-// import AddFavorites from './components/AddFavorites';
-// import RemoveFavorites from './components/RemoveFavorites';
-// import MovieListHeading from './components/MovieListHeading';
+
 import MainHeading from './components/MainHeading';
 import CSVMovieList from './components/CSVMovieList';
 import CSVSoonList from './components/CSVSoonList';
 import MoviePace from './components/MoviePace';
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
-  const [favorites, setFavorites] = useState([]);
-
-  const getMovieRequest = async (searchValue) => {
-    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=8e1cae8c`;
-    const response = await fetch(url);
-    const responseJson = await response.json();
-    if (responseJson.Search)
-      setMovies(responseJson.Search);
-  };
-
-  useEffect(() => {
-    getMovieRequest(searchValue);
-  }, [searchValue]);
-
-  useEffect(() => {
-    const movieFavorites = JSON.parse(localStorage.getItem('react-movie-app-favorites-1'));
-    setFavorites(movieFavorites || []); // Provides an empty array fallback if null
-  }, []);
-
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem('react-movie-app-favorites-1', JSON.stringify(items))
-  }
-
-  const addFavoriteMovie = (movie) => {
-    const isAlreadyFavorite = favorites.some(favorite => favorite.imdbID === movie.imdbID);
-    if (!isAlreadyFavorite) {
-      const newFavoriteList = [...favorites, movie];
-      setFavorites(newFavoriteList);
-      saveToLocalStorage(newFavoriteList);
-    }
-  }
-
-  const removeFavoriteMovie = (movie) => {
-    const newFavoriteList = favorites.filter((favorite) => favorite.imdbID !== movie.imdbID);
-    setFavorites(newFavoriteList);
-    saveToLocalStorage(newFavoriteList);
-  }
-
   return (
     <>
       <div className="home-header">
@@ -68,41 +23,6 @@ const App = () => {
       <CSVMovieList heading="Haifa Cinemateque" />
       <CSVMovieList heading="Jaffa Cinema" />
       <CSVSoonList heading="Coming Soon" />
-
-      {/* <div className="home-header">
-        <MovieListHeading heading="Search" />
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-      </div>
-      <div className="movie-row-1">
-        <div className="pre-row">
-          <MovieList
-            movies={movies}
-            handleFavoritesClick={addFavoriteMovie}
-            favoriteComponent={AddFavorites} />
-        </div>
-      </div>
-      <div className="home-header">
-        <MovieListHeading heading="Favorites" />
-      </div>
-      <div className="movie-row-1">
-        <div className="pre-row">
-          <MovieList
-            movies={favorites}
-            handleFavoritesClick={removeFavoriteMovie}
-            favoriteComponent={RemoveFavorites} />
-        </div>
-      </div>
-      <div className="movie-row-1">
-        <div className="vertical-row-1">
-          <h1>Your Favorites</h1>
-          <br></br>
-          <VerticalFavorites
-            movies={favorites}
-            handleFavoritesClick={removeFavoriteMovie}
-            favoriteComponent={RemoveFavorites} />
-        </div>
-      </div> */}
-      
     </>
   );
 }
