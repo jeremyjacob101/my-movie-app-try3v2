660de5220175c5da1300b8c9d450661ad876dd98
JJ <jeremyjacob101@gmail.com>
2024-10-01 11:19:37 +0300

    added color key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MovieCarousel from "./components/MovieCarousel";
+import CinemaColorKey from "./components/CinemaColorKey";
 
 const App = () => {
   return (
     <>
       <div className="main-carousel-holder">
+        <CinemaColorKey />
         <MovieCarousel />
       </div>
     </>
---
+++ src/components/CinemaColorKey.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../CinemaColorKey.css"; // Optional CSS file for styling
+
+const CinemaColorKey = () => {
+  return (
+    <div className="cinema-key">
+      <div className="key-item">
+        <div className="showtime-time yes-planet"></div>
+        <span>Yes Planet</span>
+      </div>
+      <div className="key-item">
+        <div className="showtime-time cinema-city"></div>
+        <span>Cinema City</span>
+      </div>
+      <div className="key-item">
+        <div className="showtime-time lev-cinema"></div>
+        <span>Lev Cinema</span>
+      </div>
+    </div>
+  );
+};
+
+export default CinemaColorKey;
